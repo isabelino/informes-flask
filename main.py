8836aec2e5d79f7fb2bf412ff87f4b7e365d894e
Dario Garcia <dario61081@gmail.com>
2023-12-22 12:15:56 -0300

    propuesta: modelos de la base de datos

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
 from dotenv import load_dotenv
 from services_info import app, Routes
-
+from flask_swagger_ui import get_swaggerui_blueprint
 
 load_dotenv()  # load .env file to environment
 
 routes = Routes(app)  # load routes
 
+SWAGGER_URL="/swagger"
+API_URL="/static/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config = {
+        'app_name': "informes-senave"
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 
 
